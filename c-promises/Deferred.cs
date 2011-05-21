bc4eaebf92b1e408b49bc4570bad5e938ebaf165
Matt Kotsenas <matt.kotsenas@gmail.com>
2011-05-21 13:19:49 -0700

    Initial checkin.

---
+++ c-promises/Deferred.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace c_promises
+{
+    public class Deferred
+    {
+        public Deferred always()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred done()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred fail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean isRejected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean isResolved()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred pipe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Promise promise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred reject()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred resolve()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Deferred then()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ c-promises/Promise.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace c_promises
+{
+    class Promise
+    {
+        public Promise then()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Promise done()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Promise fail()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Promise always()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Promise pipe()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean isResolved()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Boolean isRejected()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
