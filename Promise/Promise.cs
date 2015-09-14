fa1e71e529cc940e8fe801776b4ce9b0d39bf7fd
Dibran Mulder <superdib88@hotmail.com>
2015-09-14 11:48:15 +0200

    Renamed c-promise to Promise
    C-promise is a stupid name.. Renamed all references, folders and
    namespace.

---
+++ Promise/Promise.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Promise
+{
+    public interface Promise
+    {
+        Promise always(Delegate callback);
+        Promise always(IEnumerable<Delegate> callbacks);
+
+        Promise done(Delegate callback);
+        Promise done(IEnumerable<Delegate> callbacks);
+
+        Promise fail(Delegate callback);
+        Promise fail(IEnumerable<Delegate> callbacks);
+
+        bool isRejected();
+        bool isResolved();
+
+        Promise pipe(Delegate doneFilter, Delegate failFilter);
+
+        Promise then(Delegate doneCallback, Delegate failCallback);
+        Promise then(IEnumerable<Delegate> doneCallbacks, IEnumerable<Delegate> failCallbacks);
+    }
+}
